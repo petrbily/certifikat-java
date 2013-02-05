d01385ca28cdb91e6e1b12bd912f71d855c5c8a6
petr.bily@gmail.com <petr.bily@gmail.com@b59c51d5-8360-347f-51fd-72a500b3bdc0>
2013-02-05 23:51:33 +0000

    Druhy commit

--- trunk/src/test1/Test1.java
+++ trunk/src/test1/Test1.java
@@ -1,5 +1,7 @@
 package test1;
 
+import java.util.ArrayList;
+
 public class Test1 {
 
 	/**
@@ -7,6 +9,13 @@ public class Test1 {
 	 */
 	public static void main(String[] args) {
 		System.out.println("ahoj");
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		
+		System.out.println(list.hashCode());
 	}
 
 }
